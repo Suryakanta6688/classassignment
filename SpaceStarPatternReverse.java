d01d9c3420ef9724ed7d77331fea7b8b1647fca1
Suryakanta6688 <117418462+Suryakanta6688@users.noreply.github.com>
2022-11-10 10:09:21 +0530

    Add files via upload

---
+++ SpaceStarPatternReverse.java
@@ -0,0 +1,26 @@
+package javabasicprogram;
+
+public class SpaceStarPatternReverse {
+
+	public static void main(String[] args) {
+int i,j,k;
+
+	for(i=1;i<=5;i++)
+	{
+		for(j=1;j<i;j++)
+		{
+			System.out.print(" ");
+			}
+		
+		for(k=5;k>=i;k--)
+		{
+			System.out.print(" *");
+		}
+		
+		
+		System.out.println();
+	}
+	}
+}
+	
+
